1d974586088fbc5fcb5c8ddba286bdce21897dee
franlopezperez <154910836+franlopezperez@users.noreply.github.com>
2024-10-27 18:37:09 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", function() {
+  const excuseParagraph = document.getElementById("excuse");
+  window.excuseGenerator = function() {
+    const excuse = generateExcuse();
+    excuseParagraph.innerHTML = excuse;
+  };
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let ramdonWho = Math.floor(Math.random() * who.length);
+    let ramdonAction = Math.floor(Math.random() * action.length);
+    let ramdonWhat = Math.floor(Math.random() * what.length);
+    let ramdonWhen = Math.floor(Math.random() * when.length);
+
+    return (
+      who[ramdonWho] +
+      " " +
+      action[ramdonAction] +
+      " " +
+      what[ramdonWhat] +
+      " " +
+      when[ramdonWhen]
+    );
+  }
+});
